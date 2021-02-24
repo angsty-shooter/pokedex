e00df8c8690a0d5064038fe4bc0f4f1ee5d83da8
Ashley Herrera <ashleyherrera131@gmail.com>
2021-02-24 13:38:41 -0700

    renamed

--- app/main.js
+++ app/main.js
@@ -1,7 +1,9 @@
-import ValuesController from "./Controllers/ValuesController.js";
+// import ValuesController from "./Controllers/ValuesController.js";
 
 class App {
-  valuesController = new ValuesController();
+  //valuesController = new ValuesController();
+
+  
 }
 
 window["app"] = new App();
